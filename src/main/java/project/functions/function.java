dd95a3f10f5cc888d93b46aebf0f6bb8d05074b6
Qiyang <ngkeiyeung@gmail.com>
2021-10-10 11:50:30 +0800

    to package together, add UI, storage function in the future

--- src/main/java/project/functions/function.java
+++ src/main/java/project/functions/function.java
@@ -1,2 +1,55 @@
-package project.functions;public class function {
+package project.functions;
+
+import java.io.IOException;
+import project.ui.ui;
+import project.storage.save;
+
+public class function {
+    public boolean isExit;
+    public String description;
+    public String empty = "";
+    public String dateTimeFormat = "DD/MM/YYYY HHmm";
+    public int i = 0;
+
+    /**
+     * Return user input and initialize the status of the description
+     *
+     * @param isExit      The boolean value if the exit condition is true.
+     * @param description The task description/command from user.
+     */
+    public function(boolean isExit, String description) {
+        this.isExit = isExit;
+        this.description = description;
+    }
+
+
+    public String empty(String empty) {
+        return this.empty = empty;
+    }
+
+    public int i(int i) {
+        return this.i = i;
+    }
+    /**
+     * to decide true or false
+     *
+     * @return boolean
+     */
+    public boolean isExit() {
+        return isExit;
+    }
+
+    /**
+     * To give a warning message that if the function does not exist.
+     *
+     * @param todoList
+     * @param ui
+     * @param storage
+     * @throws Exception
+     * @throws IOException
+     */
+    public void warningMessage(TodoList todoList, ui ui, save storage) throws Exception, IOException {
+        throw new Exception("The function was not found. Please try again!");
+    }
+
 }
