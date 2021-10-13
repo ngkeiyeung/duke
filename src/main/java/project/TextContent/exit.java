722e332dc2cb3dbcf9ef467451c54149044efdcc
Qiyang <ngkeiyeung@gmail.com>
2021-10-13 20:17:11 +0800

    update exit function when key in bye

--- src/main/java/project/TextContent/exit.java
+++ src/main/java/project/TextContent/exit.java
@@ -1,4 +1,22 @@
-package TextContent;
+package project.TextContent;
 
-public class exit {
+import project.functions.TodoList;
+import project.functions.function;
+
+public class
+exit extends function {
+
+    /**
+     * Return user input and initialize the status of the description
+     *
+     * @param isExit      The boolean value if the exit condition is true.
+     * @param description The task description/command from user.
+     */
+    public exit(boolean isExit, String description) {
+        super(isExit, description);
+    }
+
+    public void exe(TodoList todoList, ui ui, save storage) {
+        ui.exitMessage();
+    }
 }
